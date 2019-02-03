3aef7bdc76045c0be52aca63e24760437aeca49e
Dmitry Torba <dmitrytorba@gmail.com>
2019-02-03 12:30:51 -0800

    mux api

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 //	"encoding/json"
     "log"
-    //"net/http"
-    //"github.com/gorilla/mux"
+    "net/http"
+    "github.com/gorilla/mux"
  	"database/sql"
     "fmt"
      _ "github.com/lib/pq"
@@ -15,6 +15,8 @@ const (
     DB_PASSWORD = "password"
     DB_NAME     = "geogeist"
 )
+
+var db sql.DB
  
 func checkErr(err error) {
     if err != nil {
@@ -22,28 +24,41 @@ func checkErr(err error) {
     }
 }
 
-func main() {
+func connectDb() {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
             DB_USER, DB_PASSWORD, DB_NAME)
     db, err := sql.Open("postgres", dbinfo)
     checkErr(err)
     defer db.Close()
+}
 
+func GetLocation(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	lon := params["lon"]
+	lat := params["lat"]
+	log.Println(lon)
+	log.Println(lat)
+	
     // using $1 syntax throws invalid geometry error
     // TODO figure out why
     coords := "-121.2273314 38.6950877"
     row := db.QueryRow("SELECT c.state FROM states c WHERE ST_Covers(c.geog, 'SRID=4326;POINT(" + coords + ")'::geography)")
-    checkErr(err)
     var stateFips string
-    err = row.Scan(&stateFips)
+    err := row.Scan(&stateFips)
     checkErr(err)
     log.Println(stateFips)
-    row = db.QueryRow("SELECT c.name FROM counties c WHERE c.state = $1 AND ST_Covers(c.geog, 'SRID=4326;POINT(" + coords + ")'::geography)", stateFips)
-    var name string
-    err = row.Scan(&name)
+    row = db.QueryRow("SELECT c.data FROM counties c WHERE c.state = $1 AND ST_Covers(c.geog, 'SRID=4326;POINT(" + coords + ")'::geography)", stateFips)
+    var data string
+    err = row.Scan(&data)
     checkErr(err)
-    log.Println(name)
+    log.Println(data)
+}
 
-    //router := mux.NewRouter()
-    //log.Fatal(http.ListenAndServe(":8000", router))
+func main() {
+	connectDb()
+    router := mux.NewRouter()
+    router.HandleFunc("/coords/{lon}/{lat}", GetLocation).Methods("GET")
+    const port = ":8000"
+    log.Println("Serving on " + port)
+    log.Fatal(http.ListenAndServe(port, router))
 }
